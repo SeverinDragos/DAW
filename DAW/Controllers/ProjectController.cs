39b5bcb1f04621852f0c8f02735a60237f361392
Severin Dragos <severin.dragos@gmail.com>
2019-12-09 17:11:45 +0200

    Added success messages

--- DAW/Controllers/ProjectController.cs
+++ DAW/Controllers/ProjectController.cs
@@ -16,6 +16,10 @@ namespace DAW.Controllers
         
         public ActionResult Index()
         {
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
             var projects = db.Projects.Include("TeamMembers");
             ViewBag.Projects = projects;
             return View();
@@ -46,30 +50,17 @@ namespace DAW.Controllers
 
                 if (ModelState.IsValid)
                 {
-                    /*var allUsers = from user in db.Users
-                                   select user;
-                    foreach (ApplicationUser userfor in allUsers)
-                    {
-                        Debug.Print(userfor.Email);
-                    }
-                    var projectManager = from user in db.Users
-                                         where user.Id == project.ProjectManagerId
-                                         select user;
-                    foreach (ApplicationUser userfor in projectManager)
-                    {
-                        Debug.Print(userfor.Email);
-                        project.TeamMembers.Add(userfor);
-                    }*/
-                    var projectManager = db.Users.Find(project.ProjectManagerId);
                     db.Projects.Add(project);
                     userManager.AddToRole(project.ProjectManagerId, "ProjectManager");
+                    var projectManager = db.Users.Find(project.ProjectManagerId);
                     project.TeamMembers.Add(projectManager);
                     db.SaveChanges();
+                    TempData["message"] = "✔ Project created successfully!";
                     return RedirectToAction("Index");
                 }
                 else
                 {
-                    return RedirectToAction("Index");
+                    return View(project);
                 }
             }
             catch (Exception e)
@@ -88,6 +79,7 @@ namespace DAW.Controllers
             }
             else
             {
+                TempData["message"] = "✘ You do not have the authority to edit this project!";
                 return RedirectToAction("Index");
             }
         }
@@ -107,11 +99,13 @@ namespace DAW.Controllers
                             project.Title = requestProject.Title;
                             project.Description = requestProject.Description;
                             db.SaveChanges();
+                            TempData["message"] = "✔ Project updated successfully!";
                         }
                         return RedirectToAction("Index");
                     }
                     else
                     {
+                        TempData["message"] = "✘ You do not have the authority to edit this project!";
                         return RedirectToAction("Index");
                     }
                 }
@@ -134,12 +128,68 @@ namespace DAW.Controllers
             {
                 db.Projects.Remove(project);
                 db.SaveChanges();
+                TempData["message"] = "✔ Project removed successfully!";
                 return RedirectToAction("Index");
             }
             else
             {
+                TempData["message"] = "✘ You do not have the authority to delete this project!";
                 return RedirectToAction("Index");
             }
+        }
+
+
+        ///TeamMembers functionality
+        
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllUsers()
+        {
+            // generam o lista goala
+            var selectList = new List<SelectListItem>();
+            // Extragem toate categoriile din baza de date
+            var users = from user in db.Users
+                             select user;
+            // iteram prin categorii
+            foreach (var user in users)
+            {
+                // Adaugam in lista elementele necesare pentru dropdown
+                selectList.Add(new SelectListItem
+                {
+                    Value = user.Id,
+                    Text = user.UserName
+                });
+            }
+            // returnam lista de categorii
+            return selectList;
+        }
+
+        public ActionResult ShowTeamMembers(int id)
+        {
+            Project project = db.Projects.Find(id);
+
+            ViewBag.AllUsers = db.Users;
+
+            return View("TeamMembers", project);
+        }
+
+        [HttpPut]
+        public ActionResult AddTeamMember(int id, string userId)
+        {
+            Project project = db.Projects.Find(id);
+            ApplicationUser user = db.Users.Find(userId);
+            ViewBag.AllUsers = db.Users;
+            TempData["message"] = "✔ Member added successfully!" + user.UserName;
+            return View("TeamMembers", project);
+        }
+
+        [HttpDelete]
+        public ActionResult RemoveTeamMember(int id, string userId)
+        {
+            Project project = db.Projects.Find(id);
+            ApplicationUser user = db.Users.Find(userId);
+            ViewBag.AllUsers = db.Users;
+            TempData["message"] = "✔ Member removed successfully!" + user.UserName;
+            return View("TeamMembers", project);
         }
     }
 }
